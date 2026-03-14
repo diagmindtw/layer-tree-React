23a4a152fe1eca00de1a9f46d29547a3d86bab90
diagmindtw <diagmindtw@gmail.com>
2026-03-14 21:00:44 +0800

    Commit from TeleportHQ

---
+++ src/views/google-pixel21.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './google-pixel21.css'
+
+const GooglePixel21 = (props) => {
+  return (
+    <div className="google-pixel21-container">
+      <Helmet>
+        <title>exported project</title>
+        <meta property="og:title" content="exported project" />
+        <link
+          rel="canonical"
+          href="https://layer-tree-41ty0b.teleporthq.site/"
+        />
+        <meta
+          property="og:url"
+          content="https://layer-tree-41ty0b.teleporthq.site/"
+        />
+      </Helmet>
+      <div className="google-pixel21-thq-google-pixel21-elm">
+        <div className="google-pixel21-thq-frame1-elm">
+          <div className="google-pixel21-thq-frame3-elm">
+            <div className="google-pixel21-thq-frame5-elm">
+              <span className="google-pixel21-thq-text-elm1">儲存</span>
+            </div>
+            <div className="google-pixel21-thq-frame4-elm">
+              <span className="google-pixel21-thq-text-elm2">新增頁面</span>
+            </div>
+          </div>
+          <div className="google-pixel21-thq-frame2-elm">
+            <span className="google-pixel21-thq-text-elm3">樹狀頁面編輯</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default GooglePixel21
